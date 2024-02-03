b5a4d9562b0c902c463d9ceb0eba0471533042fc
Alex Ditu <aditu@bitdefender.com>
2024-02-03 13:43:30 +0200

    build.sh: configure major and minor versions from build script

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ set -x
 LD_FLAGS="\
 -X github.com/alexditu/go-utils/pkg/version.commitHash=$(git rev-parse --short HEAD) \
 -X github.com/alexditu/go-utils/pkg/version.branchName=$(git rev-parse --abbrev-ref HEAD) \
--X github.com/alexditu/go-utils/pkg/version.binaryName=${BIN_NAME}"
+-X github.com/alexditu/go-utils/pkg/version.binaryName=${BIN_NAME} \
+-X github.com/alexditu/go-utils/pkg/version.major=1 \
+-X github.com/alexditu/go-utils/pkg/version.minor=0"
 
 GOARCH=amd64 GOOS=linux \
 go build -x -o "${BIN_DIR}/${BIN_NAME}" \
